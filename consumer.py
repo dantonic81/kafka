007a8d00fcbf0a58bd7fa272a7e3e0cfb4c55df4
Dean <imadeureadis@gmail.com>
2024-12-30 17:59:42 +0100

    set correct bootstrap and add logging

--- consumer.py
+++ consumer.py
@@ -1,15 +1,16 @@
 from kafka import KafkaConsumer
 import json
+from utils.logger import logger
 
 # Configure the consumer
 consumer = KafkaConsumer(
     'portfolio-topic',  # The topic name
-    bootstrap_servers=['localhost:9092'],  # Replace with your Kafka broker address
+    bootstrap_servers=['lovely-candice-acmetest-e2699be0.koyeb.app:9092'],  # Replace with your Kafka broker address
     group_id='portfolio-consumer-group',
     value_deserializer=lambda x: x.decode('utf-8') if x else None  # Avoid decoding empty messages
 )
 
-print("Consumer is listening to the topic 'portfolio-topic'... v2")
+logger.info("Consumer is listening to the topic 'portfolio-topic'...")
 
 # Poll for messages from the Kafka topic
 for message in consumer:
@@ -17,16 +18,16 @@ for message in consumer:
         # Attempt to deserialize the message
         if message.value:
             deserialized_message = json.loads(message.value)  # Deserialize the JSON string
-            print(f"Deserialized message: {deserialized_message}")
+            logger.info(f"Deserialized message: {deserialized_message}")
         else:
-            print("Received an empty message. Skipping...")
+            logger.info("Received an empty message. Skipping...")
 
     except json.JSONDecodeError as e:
         # Handle JSON decode errors gracefully
-        print(f"Error decoding message: {e}. Skipping the message.")
+        logger.error(f"Error decoding message: {e}. Skipping the message.")
         continue  # Skip this message and continue consuming
 
     except Exception as e:
         # Handle any other exceptions that might occur
-        print(f"Unexpected error: {e}. Skipping the message.")
+        logger.error(f"Unexpected error: {e}. Skipping the message.")
         continue  # Skip this message and continue consuming
