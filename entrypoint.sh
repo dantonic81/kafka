ef4425b1c283d940076b937fc7828426faaf0b08
Dean <imadeureadis@gmail.com>
2024-12-31 10:41:57 +0100

    replace existing listener values

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Default to Koyeb's URL if not set
-export KAFKA_ADVERTISED_LISTENERS="${KAFKA_ADVERTISED_LISTENERS:-PLAINTEXT://lovely-candice-acmetest-e2699be0.koyeb.app:9092}"
-export KAFKA_LISTENERS="${KAFKA_LISTENERS:-PLAINTEXT://0.0.0.0:9092}"
+#export KAFKA_ADVERTISED_LISTENERS="${KAFKA_ADVERTISED_LISTENERS:-PLAINTEXT://lovely-candice-acmetest-e2699be0.koyeb.app:9092}"
+#export KAFKA_LISTENERS="${KAFKA_LISTENERS:-PLAINTEXT://0.0.0.0:9092}"
 
 # Set lower memory limits for Kafka
 export KAFKA_HEAP_OPTS="-Xmx256m -Xms128m"  # Adjust this based on available memory
@@ -17,8 +17,11 @@ fi
 # Set Kafka configuration dynamically
 echo "Configuring Kafka listeners..."
 echo "controller.listener.names=CONTROLLER" >> /opt/bitnami/kafka/config/kraft/server.properties
-echo "listeners=PLAINTEXT://0.0.0.0:9092,CONTROLLER://0.0.0.0:9093" >> /opt/bitnami/kafka/config/kraft/server.properties
-echo "advertised.listeners=PLAINTEXT://lovely-candice-acmetest-e2699be0.koyeb.app:9092,CONTROLLER://localhost:9093" >> /opt/bitnami/kafka/config/kraft/server.properties
+# Replace existing listeners
+sed -i "s|^listeners=.*|listeners=PLAINTEXT://0.0.0.0:9092,CONTROLLER://0.0.0.0:9093|" /opt/bitnami/kafka/config/kraft/server.properties
+
+# Replace existing advertised.listeners
+sed -i "s|^advertised.listeners=.*|advertised.listeners=PLAINTEXT://lovely-candice-acmetest-e2699be0.koyeb.app:9092,CONTROLLER://localhost:9093|" /opt/bitnami/kafka/config/kraft/server.properties
 
 echo "Verifying controller.listener.names and listeners in server.properties:"
 cat /opt/bitnami/kafka/config/kraft/server.properties | grep -E "listeners|controller.listener.names"
