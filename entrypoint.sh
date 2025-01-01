9d94980128ff0782797fd3711da87aa37c6d3020
Dean <imadeureadis@gmail.com>
2025-01-01 13:38:06 +0100

    test with sed

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,10 +18,10 @@ fi
 echo "Configuring Kafka listeners..."
 echo "controller.listener.names=CONTROLLER" >> /opt/bitnami/kafka/config/kraft/server.properties
 # Replace existing listeners
-#sed -i "s|^listeners=.*|listeners=PLAINTEXT://0.0.0.0:9092,CONTROLLER://0.0.0.0:9093|" /opt/bitnami/kafka/config/kraft/server.properties
-#
-## Replace existing advertised.listeners
-#sed -i "s|^advertised.listeners=.*|advertised.listeners=PLAINTEXT://lovely-candice-acmetest-e2699be0.koyeb.app:9092,CONTROLLER://localhost:9093|" /opt/bitnami/kafka/config/kraft/server.properties
+sed -i "s|^listeners=.*|listeners=PLAINTEXT://0.0.0.0:9092,CONTROLLER://0.0.0.0:9093|" /opt/bitnami/kafka/config/kraft/server.properties
+
+# Replace existing advertised.listeners
+sed -i "s|^advertised.listeners=.*|advertised.listeners=PLAINTEXT://lovely-candice-acmetest-e2699be0.koyeb.app:9092,CONTROLLER://localhost:9093|" /opt/bitnami/kafka/config/kraft/server.properties
 
 echo "Verifying controller.listener.names and listeners in server.properties:"
 cat /opt/bitnami/kafka/config/kraft/server.properties | grep -E "listeners|controller.listener.names"
